c154b359d880960892508ab1f4ffa59d42d5217e
Richard Bennett <richard@tailoredpos.com>
2015-01-04 07:09:23 -0500

    Basic example of a self-hosted nancyfx app.

--- source/DataHub.ServiceHost/Service1.cs
+++ source/DataHub.ServiceHost/Service1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Owin.Hosting;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,14 +11,36 @@ using System.Threading.Tasks;
 
 namespace DataHub.ServiceHost {
     public partial class Service1 : ServiceBase {
+        public static void Main() {
+            if (Environment.UserInteractive) {
+                var bootstrapper = new Service1();
+                bootstrapper.OnStart(new string[0]);
+                Console.WriteLine("Press any key to stop program.");
+                Console.ReadLine();
+                bootstrapper.OnStop();
+            } else {
+                ServiceBase[] ServicesToRun;
+                ServicesToRun = new ServiceBase[] 
+            { 
+                new Service1() 
+            };
+                ServiceBase.Run(ServicesToRun);
+            }
+        }
+
+        IDisposable _WebApp;
+        string _Url = "http://+:8080";
+
         public Service1() {
             InitializeComponent();
         }
 
         protected override void OnStart(string[] args) {
+            _WebApp = WebApp.Start<Startup>(_Url);
         }
 
         protected override void OnStop() {
+            _WebApp.Dispose();
         }
     }
 }
