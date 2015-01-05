5a0ecb299f72d498824cda91cd494356df77e565
Richard Bennett <richard@tailoredpos.com>
2015-01-05 18:31:07 -0500

    All nuget packages are building as expected.

---
+++ source/ClientApp.WinHost/Sample.cs
@@ -0,0 +1,16 @@
+﻿using Squirrel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientApp.WinHost {
+    class Sample {
+        public async void Task() {
+            using (var mgr = new UpdateManager(urlOrPath: "", applicationName: "", appFrameworkVersion: Squirrel.FrameworkVersion.Net45)) {
+                var re = await mgr.UpdateApp();
+            }
+        }
+    }
+}
--- source/DataHub.ServiceHost/Startup.cs
+++ source/DataHub.ServiceHost/Startup.cs
@@ -1,4 +1,5 @@
 ﻿using Owin;
+using Squirrel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,18 @@ namespace DataHub.ServiceHost {
     public class Startup {
         public void Configuration(IAppBuilder app) {
             app.UseNancy();
+        }
+    }
+
+    public static class UpdateProcedure {
+        public static void TryToUpdate() {
+            using (var mgr = new UpdateManager(urlOrPath: "", applicationName: "", appFrameworkVersion: FrameworkVersion.Net45)) {
+                SquirrelAwareApp.HandleEvents(
+                    onInitialInstall: v => mgr.CreateShortcutForThisExe(),
+                    onAppUpdate: v => mgr.CreateShortcutForThisExe(),
+                    onAppUninstall: v => mgr.RemoveShortcutForThisExe()
+                );
+            }
         }
     }
 }
