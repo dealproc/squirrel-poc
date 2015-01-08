1d8c65d9b29e9e94e53b1f0e79c68abc6570b325
Richard Bennett <richard@tailoredpos.com>
2015-01-08 14:31:48 -0500

    Added /Releases/RELEASES path.

--- source/DataHub.ServiceHost/Modules/ReleasesModule.cs
+++ source/DataHub.ServiceHost/Modules/ReleasesModule.cs
@@ -3,6 +3,7 @@ namespace DataHub.ServiceHost.Modules {
     public class ReleasesModule : NancyModule {
         public ReleasesModule() : base("/releases") {
             Get["/"] = parameters => Response.AsFile("Releases/index.html", "text/html");
+            Get["/RELEASES"] = parameters => Response.AsFile("Releases/RELEASES", "text/plain");
         }
     }
 }
--- source/DataHub.ServiceHost/Startup.cs
+++ source/DataHub.ServiceHost/Startup.cs
@@ -3,10 +3,7 @@
 namespace DataHub.ServiceHost {
     public class Startup {
         public void Configuration(IAppBuilder app) {
-            app.UseNancy((config)=>{
-                config.PerformPassThrough = context =>
-                    context.Response.StatusCode == Nancy.HttpStatusCode.NotFound;
-            });
+            app.UseNancy();
         }
     }
 }
