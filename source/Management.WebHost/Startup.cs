0336090678ed5ee90ad8ae87a4b2ce412297196a
Richard Bennett <richard@tailoredpos.com>
2015-01-07 21:37:13 -0500

    Added add'l requirements because of IIS madness.

--- source/Management.WebHost/Startup.cs
+++ source/Management.WebHost/Startup.cs
@@ -1,7 +1,9 @@
-﻿using Owin;
+﻿using Microsoft.Owin.Extensions;
+using Owin;
 
 public class Startup {
     public void Configuration(IAppBuilder app) {
         app.UseNancy();
+        app.UseStageMarker(PipelineStage.MapHandler);
     }
 }
