c0bdbc110052f985d6e7785211b4164e222d0c81
Richard Bennett <richard@tailoredpos.com>
2015-01-07 21:42:45 -0500

    added namespace back.

--- source/Management.WebHost/Startup.cs
+++ source/Management.WebHost/Startup.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.Owin.Extensions;
 using Owin;
 
-public class Startup {
-    public void Configuration(IAppBuilder app) {
-        app.UseNancy();
-        app.UseStageMarker(PipelineStage.MapHandler);
+namespace Management.WebHost {
+    public class Startup {
+        public void Configuration(IAppBuilder app) {
+            app.UseNancy();
+            app.UseStageMarker(PipelineStage.MapHandler);
+        }
     }
 }
