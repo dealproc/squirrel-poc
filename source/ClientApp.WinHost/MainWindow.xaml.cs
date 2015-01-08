563737e2a5947ef7d6f81e77afb8139c0d92ab04
Richard Bennett <richard@tailoredpos.com>
2015-01-08 14:09:18 -0500

    first run of enabling updates.

--- source/ClientApp.WinHost/MainWindow.xaml.cs
+++ source/ClientApp.WinHost/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Squirrel;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,8 +21,56 @@ namespace ClientApp.WinHost {
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
+        UpdateInfo updateInfo = null;
+        UpdateManager updateMgr = null;
         public MainWindow() {
             InitializeComponent();
+            var settings = new RegistrySettings();
+
+            txtUpdateUrl.TextChanged += (s, e) => {
+                settings.UpdateUrl = (s as TextBox).Text;
+                if (updateMgr != null) {
+                    updateMgr.Dispose();
+                    updateMgr = null;
+                }
+            };
+
+            btnCheckForUpdates.Click += async (s, e) => {
+                updateMgr = new UpdateManager(settings.UpdateUrl, settings.PackageId, FrameworkVersion.Net45);
+
+                txtSystemMessages.AppendText("Checking for updates" + Environment.NewLine);
+                updateInfo = await updateMgr.CheckForUpdate();
+
+                if (updateInfo == null) {
+                    txtSystemMessages.AppendText("No updates were found." + Environment.NewLine + Environment.NewLine);
+                } else if (!updateInfo.ReleasesToApply.Any()) {
+                    txtSystemMessages.AppendText("System is up to date." + Environment.NewLine + Environment.NewLine);
+                } else {
+                    foreach (var release in updateInfo.ReleasesToApply.OrderBy(x => x.Version)) {
+                        txtSystemMessages.AppendText(string.Format("Version {0} is available." + Environment.NewLine, release.Version));
+                    }
+                }
+            };
+
+            btnDownloadUpdates.Click += async (s, e) => {
+                if (updateMgr != null && updateInfo != null) {
+                    txtSystemMessages.AppendText("Downloading updates.");
+                    await updateMgr.DownloadReleases(updateInfo.ReleasesToApply, (progess) => {
+                        txtSystemMessages.AppendText(string.Format("Download progress: {0}", progess));
+                    });
+                }
+            };
+
+            btnApplyUpdates.Click += (s, e) => {
+                updateMgr.ApplyReleases(updateInfo, (progess) => {
+                    txtSystemMessages.AppendText(string.Format("Update progress: {0}", progess));
+                });
+            };
+
+            Loaded += (s, e) => {
+                txtUpdateUrl.Text = settings.UpdateUrl;
+                txtProductVersion.Text = FileVersionInfo.GetVersionInfo(this.GetType().Assembly.Location).FileVersion;
+            };
         }
     }
 }
---
+++ source/ClientApp.WinHost/RegistrySettings.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientApp.WinHost {
+    public class RegistrySettings {
+        RegistryKey _ApplicationKey;
+        public RegistrySettings() {
+            _ApplicationKey = Registry.CurrentUser.OpenSubKey("SOFTWARE", true);
+
+            if (!_ApplicationKey.GetSubKeyNames().Any(sk => sk.Equals("Midnight Oil Systems"))) {
+                _ApplicationKey.CreateSubKey("Midnight Oil Systems");
+            }
+            _ApplicationKey = _ApplicationKey.OpenSubKey("Midnight Oil Systems", true);
+
+            if (!_ApplicationKey.GetSubKeyNames().Any(sk => sk.Equals("ClientApp"))) {
+                _ApplicationKey.CreateSubKey("ClientApp");
+            }
+            _ApplicationKey = _ApplicationKey.OpenSubKey("ClientApp", true);
+        }
+
+        public string UpdateUrl {
+            get {
+                if (_ApplicationKey.GetValueNames().Any(key => key.Equals("Update Url"))) {
+                    return _ApplicationKey.GetValue("Update Url").ToString();
+                }
+                return string.Empty;
+            }
+            set { _ApplicationKey.SetValue("Update Url", value); }
+        }
+        public string PackageId {
+            get { return "clientapp.winhost"; }
+        }
+    }
+}
