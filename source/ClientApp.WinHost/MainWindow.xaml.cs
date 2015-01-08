cfaca8227c30e9aa6a5fc9169837cedb2330a6aa
Richard Bennett <richard@tailoredpos.com>
2015-01-08 14:21:25 -0500

    using statements and recreating the update manager at each event.

--- source/ClientApp.WinHost/MainWindow.xaml.cs
+++ source/ClientApp.WinHost/MainWindow.xaml.cs
@@ -22,33 +22,30 @@ namespace ClientApp.WinHost {
     /// </summary>
     public partial class MainWindow : Window {
         UpdateInfo updateInfo = null;
-        UpdateManager updateMgr = null;
         public MainWindow() {
             InitializeComponent();
             var settings = new RegistrySettings();
 
             txtUpdateUrl.TextChanged += (s, e) => {
                 settings.UpdateUrl = (s as TextBox).Text;
-                if (updateMgr != null) {
-                    updateMgr.Dispose();
-                    updateMgr = null;
-                }
+                updateInfo = null;
             };
 
             btnCheckForUpdates.Click += async (s, e) => {
                 try {
-                    updateMgr = new UpdateManager(settings.UpdateUrl, settings.PackageId, FrameworkVersion.Net45);
+                    using (var updateMgr = new UpdateManager(settings.UpdateUrl, settings.PackageId, FrameworkVersion.Net45)) {
 
-                    txtSystemMessages.AppendText("Checking for updates" + Environment.NewLine);
-                    updateInfo = await updateMgr.CheckForUpdate();
+                        txtSystemMessages.AppendText("Checking for updates" + Environment.NewLine);
+                        updateInfo = await updateMgr.CheckForUpdate();
 
-                    if (updateInfo == null) {
-                        txtSystemMessages.AppendText("No updates were found." + Environment.NewLine + Environment.NewLine);
-                    } else if (!updateInfo.ReleasesToApply.Any()) {
-                        txtSystemMessages.AppendText("System is up to date." + Environment.NewLine + Environment.NewLine);
-                    } else {
-                        foreach (var release in updateInfo.ReleasesToApply.OrderBy(x => x.Version)) {
-                            txtSystemMessages.AppendText(string.Format("Version {0} is available." + Environment.NewLine, release.Version));
+                        if (updateInfo == null) {
+                            txtSystemMessages.AppendText("No updates were found." + Environment.NewLine + Environment.NewLine);
+                        } else if (!updateInfo.ReleasesToApply.Any()) {
+                            txtSystemMessages.AppendText("System is up to date." + Environment.NewLine + Environment.NewLine);
+                        } else {
+                            foreach (var release in updateInfo.ReleasesToApply.OrderBy(x => x.Version)) {
+                                txtSystemMessages.AppendText(string.Format("Version {0} is available." + Environment.NewLine, release.Version));
+                            }
                         }
                     }
                 } catch (Exception ex) {
@@ -57,23 +54,27 @@ namespace ClientApp.WinHost {
             };
 
             btnDownloadUpdates.Click += async (s, e) => {
-                if (updateMgr != null && updateInfo != null) {
-                    try {
-                        txtSystemMessages.AppendText("Downloading updates.");
-                        await updateMgr.DownloadReleases(updateInfo.ReleasesToApply, (progess) => {
-                            txtSystemMessages.AppendText(string.Format("Download progress: {0}", progess));
-                        });
-                    } catch (Exception ex) {
-                        AppendMessage(ex);
+                if (updateInfo != null) {
+                    using (var updateMgr = new UpdateManager(settings.UpdateUrl, settings.PackageId, FrameworkVersion.Net45)) {
+                        try {
+                            txtSystemMessages.AppendText("Downloading updates.");
+                            await updateMgr.DownloadReleases(updateInfo.ReleasesToApply, (progess) => {
+                                txtSystemMessages.AppendText(string.Format("Download progress: {0}", progess));
+                            });
+                        } catch (Exception ex) {
+                            AppendMessage(ex);
+                        }
                     }
                 }
             };
 
             btnApplyUpdates.Click += (s, e) => {
                 try {
-                    updateMgr.ApplyReleases(updateInfo, (progess) => {
-                        txtSystemMessages.AppendText(string.Format("Update progress: {0}", progess));
-                    });
+                    using (var updateMgr = new UpdateManager(settings.UpdateUrl, settings.PackageId, FrameworkVersion.Net45)) {
+                        updateMgr.ApplyReleases(updateInfo, (progess) => {
+                            txtSystemMessages.AppendText(string.Format("Update progress: {0}", progess));
+                        });
+                    }
                 } catch (Exception ex) {
                     AppendMessage(ex);
                 }
--- source/DataHub.ServiceHost/NancyBootstrapperWithCustomizations.cs
+++ source/DataHub.ServiceHost/NancyBootstrapperWithCustomizations.cs
@@ -4,7 +4,7 @@ namespace DataHub.ServiceHost {
     public class NancyBootstrapperWithCustomizations : DefaultNancyBootstrapper {
         protected override void ConfigureConventions(Nancy.Conventions.NancyConventions nancyConventions) {
             base.ConfigureConventions(nancyConventions);
-            Conventions.StaticContentsConventions.AddDirectory("/releases");
+            Conventions.StaticContentsConventions.AddDirectory("/Releases");
             Conventions.StaticContentsConventions.AddDirectory("/Content");
             Conventions.StaticContentsConventions.AddDirectory("/fonts");
             Conventions.StaticContentsConventions.AddDirectory("/Scripts");
