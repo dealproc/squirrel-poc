c1be36a534b18b4f0e5309946d2a877023873937
Richard Bennett <richard@tailoredpos.com>
2015-01-07 22:24:35 -0500

    Added bin deployment as part of the nuspec file.

--- source/Management.WebHost/Startup.cs
+++ source/Management.WebHost/Startup.cs
@@ -1,9 +1,11 @@
-﻿using Owin;
+﻿using Microsoft.Owin.Extensions;
+using Owin;
 
 namespace Management.WebHost {
     public class Startup {
         public void Configuration(IAppBuilder app) {
             app.UseNancy();
+            app.UseStageMarker(PipelineStage.MapHandler);
         }
     }
 }
