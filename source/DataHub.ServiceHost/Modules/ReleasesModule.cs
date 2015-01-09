e655b22dc3f97e0bb8c125bdf82e8bdea1749d11
Richard Bennett <richard@tailoredpos.com>
2015-01-08 23:21:40 -0500

    have the RELEASES file serving.

--- source/DataHub.ServiceHost/Modules/ReleasesModule.cs
+++ source/DataHub.ServiceHost/Modules/ReleasesModule.cs
@@ -1,9 +1,11 @@
 ﻿using Nancy;
+using System.IO;
+using System.Reflection;
 namespace DataHub.ServiceHost.Modules {
     public class ReleasesModule : NancyModule {
-        public ReleasesModule() : base("/releases") {
-            Get["/"] = parameters => Response.AsFile("Releases/index.html", "text/html");
-            Get["/RELEASES"] = parameters => Response.AsFile("Releases/RELEASES", "text/plain");
+        public ReleasesModule() : base("/install") {
+            Get[""] = parameters => Response.AsFile("Releases/index.html", "text/html");
+            Get["releases"] = parameters => Response.FromStream(File.OpenRead("./Releases/RELEASES"), "text/plain");
         }
     }
 }
