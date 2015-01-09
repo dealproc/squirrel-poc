ba01cd0c30fc95904704e0152156190063e701ea
Richard Bennett <richard@tailoredpos.com>
2015-01-09 00:20:10 -0500

    enabling the use of the Owin StaticFiles extension.. need to report a bug to the Nancy team to see if they can provide a similar flag that the StaticFiles extension uses to serve files of an unknown type.

--- source/DataHub.ServiceHost/NancyBootstrapperWithCustomizations.cs
+++ source/DataHub.ServiceHost/NancyBootstrapperWithCustomizations.cs
@@ -7,16 +7,9 @@ namespace DataHub.ServiceHost {
         protected override void ConfigureConventions(Nancy.Conventions.NancyConventions nancyConventions) {
             base.ConfigureConventions(nancyConventions);
 
-            Conventions.StaticContentsConventions.AddDirectory("/Releases");
             Conventions.StaticContentsConventions.AddDirectory("/Content");
             Conventions.StaticContentsConventions.AddDirectory("/fonts");
             Conventions.StaticContentsConventions.AddDirectory("/Scripts");
-
-            Conventions.StaticContentsConventions.AddFile("/releases/RELEASES", Path.Combine(
-                Path.GetDirectoryName(
-                    Assembly.GetExecutingAssembly().Location
-                ), "Releases/RELEASES")
-            );
         }
     }
 }
--- source/DataHub.ServiceHost/Startup.cs
+++ source/DataHub.ServiceHost/Startup.cs
@@ -1,8 +1,25 @@
-﻿using Owin;
+﻿using Microsoft.Owin;
+using Microsoft.Owin.FileSystems;
+using Microsoft.Owin.StaticFiles;
+using Owin;
+using System.IO;
+using System.Reflection;
 
 namespace DataHub.ServiceHost {
     public class Startup {
         public void Configuration(IAppBuilder app) {
+            app.UseStaticFiles(new StaticFileOptions {
+                RequestPath = new PathString("/releases"),
+                FileSystem = new PhysicalFileSystem(
+                    Path.Combine(
+                        Path.GetDirectoryName(
+                            Assembly.GetExecutingAssembly().Location
+                        ), 
+                        "Releases"
+                    )
+                ),
+                ServeUnknownFileTypes = true
+            });
             app.UseNancy();
             //StaticConfiguration.DisableErrorTraces = false;
         }
