6fb87b2b20d3ef98fcf9225de10193908cc56838
Richard Bennett <richard@tailoredpos.com>
2015-01-08 23:34:10 -0500

    reworked discovery of folder where files were located.

--- source/DataHub.ServiceHost/Startup.cs
+++ source/DataHub.ServiceHost/Startup.cs
@@ -5,7 +5,7 @@ namespace DataHub.ServiceHost {
     public class Startup {
         public void Configuration(IAppBuilder app) {
             app.UseNancy();
-            StaticConfiguration.DisableErrorTraces = false;
+            //StaticConfiguration.DisableErrorTraces = false;
         }
     }
 }
